cfe104ec319c38e177352f362971b35e81d34a01
raphael <raphael.sculati@gmail.com>
2016-01-27 19:46:58 +0900

    bower comp

---
+++ platforms/android/assets/www/scripts/app.js
@@ -0,0 +1,209 @@
+'use strict';
+angular.module('main', [
+  'ionic',
+  'ngCordova',
+  'ui.router',
+  // TODO: load other modules selected during generation
+])
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  // ROUTING with ui.router
+  $urlRouterProvider.otherwise('/main/home');
+  $stateProvider
+    // this state is placed in the <ion-nav-view> in the index.html
+    .state('main', {
+      url: '/main',
+      abstract: true,
+      templateUrl: 'main/templates/menu.html',
+      controller: 'MenuCtrl as menu'
+    })
+    .state('main.home', {
+      url: '/home',
+      views: {
+        'pageContent': {
+          templateUrl: 'main/templates/home.html',
+          controller: 'HomeCtrl as menu'
+        }
+      }
+    })
+    .state('main.categories', {
+      url: '/categories',
+      views: {
+        'pageContent': {
+          templateUrl: 'main/templates/categorie.html',
+          controller: 'CategoriesCtrl as menu'
+        }
+      }
+    })
+      .state('main.list', {
+        url: '/list',
+        views: {
+          'pageContent': {
+            templateUrl: 'main/templates/list.html',
+            controller: 'ListCtrl as menu'
+          }
+        }
+      })
+      .state('main.listDetail', {
+        url: '/list/detail',
+        views: {
+          'pageContent': {
+            templateUrl: 'main/templates/list-detail.html',
+            // controller: '<someCtrl> as ctrl'
+          }
+        }
+      })
+      .state('main.debug', {
+        url: '/debug',
+        views: {
+          'pageContent': {
+            templateUrl: 'main/templates/debug.html',
+            controller: 'DebugCtrl as ctrl'
+          }
+        }
+      });
+});
+
+'use strict';
+angular.module('main')
+.service('Main', function ($log, $timeout) {
+
+  $log.log('Hello from your Service: Main in module main');
+
+  // some initial data
+  this.someData = {
+    binding: 'Yes! Got that databinding working'
+  };
+
+  this.changeBriefly = function () {
+    var initialValue = this.someData.binding;
+    this.someData.binding = 'Yeah this was changed';
+
+    var that = this;
+    $timeout(function () {
+      that.someData.binding = initialValue;
+    }, 500);
+  };
+
+});
+
+'use strict';
+angular.module('main')
+.controller('MenuCtrl', function ($log) {
+
+  $log.log('Hello from your Controller: MenuCtrl in module main:. This is your controller:', this);
+
+});
+
+'use strict';
+angular.module('main')
+.controller('ListCtrl', function ($scope) {
+
+  $scope.itemsList = [];
+
+  /**
+   * fills the itemsList variable with data to be displayed on the page
+   */
+  function populateList () {
+      // adds a new element (specified JSON object) to $scope.itemsList
+    $scope.itemsList.push({'name': 'Tanah-lot Temple'});
+    $scope.itemsList.push({'name': 'Mount Batur'});
+    $scope.itemsList.push({'name': 'Ulun Danu Temple'});
+    $scope.itemsList.push({'name': 'Uluwatu Temple'});
+  }
+
+  // call function to populate list
+  populateList();
+
+});
+
+'use strict';
+angular.module('main')
+.controller('HomeCtrl', function ($state, $scope, $ionicHistory) {
+
+  $scope.changeState = function () {
+
+    $ionicHistory.nextViewOptions({
+      disableAnimate: true
+    });
+
+    $state.go('main.categories');
+  };
+
+});
+
+'use strict';
+angular.module('main')
+.controller('DebugCtrl', function ($log, Main, Config, $cordovaDevice) {
+
+  $log.log('Hello from your Controller: DebugCtrl in module main:. This is your controller:', this);
+
+  // bind data from services
+  this.someData = Main.someData;
+  this.ENV = Config.ENV;
+  this.BUILD = Config.BUILD;
+  // get device info
+  ionic.Platform.ready(function () {
+    if (ionic.Platform.isWebView()) {
+      this.device = $cordovaDevice.getDevice();
+    }
+  }.bind(this));
+
+  // PASSWORD EXAMPLE
+  this.password = {
+    input: '', // by user
+    strength: ''
+  };
+  this.grade = function () {
+    var size = this.password.input.length;
+    if (size > 8) {
+      this.password.strength = 'strong';
+    } else if (size > 3) {
+      this.password.strength = 'medium';
+    } else {
+      this.password.strength = 'weak';
+    }
+  };
+  this.grade();
+
+});
+
+'use strict';
+angular.module('main')
+.controller('CategoriesCtrl', function ($state, $scope, $ionicHistory) {
+
+  $scope.changeState = function () {
+
+    $ionicHistory.nextViewOptions({
+      disableAnimate: true
+    });
+
+    $state.go('main.list');
+  };
+
+});
+
+'use strict';
+angular.module('main')
+.constant('Config', {
+
+  // gulp environment: injects environment vars
+  ENV: {
+    /*inject-env*/
+    'SERVER_URL': 'https://DEVSERVER/api'
+    /*endinject*/
+  },
+
+  // gulp build-vars: injects build vars
+  BUILD: {
+    /*inject-build*/
+    /*endinject*/
+  }
+
+});
+
+'use strict';
+angular.module('memorable', [
+  // load your modules here
+  'main' // starting with the main module
+]);
---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-device": "1.1.0",
+    "cordova-plugin-statusbar": "2.0.0"
+}
+// BOTTOM OF METADATA
+});
